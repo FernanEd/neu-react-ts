2f133c51f5368520956f470ee0ab96ea54ad98f5
fernaned <fernandoed.bm@gmail.com>
2021-05-16 19:47:44 -0500

    init js

---
+++ app/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Hola como esta</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+window.Neutralino.init();
